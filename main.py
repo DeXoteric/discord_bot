8b12841d7ccdd63ce1131530ea49df85e05cab77
DeXoteric <dexoteric@gmail.com>
2024-02-08 16:45:17 +0100

    added !ping and !8ball command

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
+import random
+
 import discord
 from discord.ext import commands
 
 import const
 
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
     print("Bot connected to Discord")
 
 
-client.run(const.TOKEN)
+@bot.command()
+async def ping(ctx):
+    bot_latency = round(bot.latency * 1000)
+    await ctx.reply(f"Bot latency: {bot_latency} ms.", mention_author=False)
+
+
+@bot.command(aliases=["8ball", "eightball", "magicball"])
+async def magic_eightball(ctx, question=""):
+    if question == "":
+        await ctx.reply("You have to ask a question", mention_author=False)
+    else:
+        with open("magic_eightball_responses.txt", "r") as f:
+            responses = f.readlines()
+            random_response = random.choice(responses)
+        await ctx.reply(random_response, mention_author=False)
+
+
+bot.run(const.TOKEN)
