8eb47a0f0a52906b4fc0a2f74bd4ccbc67c6373d
DeXoteric <dexoteric@gmail.com>
2024-02-11 17:07:29 +0100

    Implemented a new command to modify the command prefix, allowing individual servers to define their own unique prefixes.

--- main.py
+++ main.py
@@ -1,14 +1,60 @@
 import asyncio
 import discord
+import json
 import os
 from discord.ext import commands
 import config
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
 
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+def get_server_prefix(bot, message):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
+
+
+bot = commands.Bot(command_prefix=get_server_prefix, intents=intents)
+
+
+@bot.event
+async def on_guild_join(guild):
+    try:
+        with open("prefixes.json", "r") as f:
+            prefixes = json.load(f)
+    except FileNotFoundError:
+        prefixes = {}
+
+    prefixes[str(guild.id)] = "!"
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
+
+
+@bot.event
+async def on_guild_remove(guild):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def set_prefix(ctx, new_prefix: str):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = new_prefix
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
+
+    await ctx.send(f"Prefix changed to `{new_prefix}`")
 
 
 @bot.event
